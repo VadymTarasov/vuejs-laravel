9644b4aae95219e97485e2984d4bbe7f72690c45
vadym <vadym.tarasov.91@gmail.com>
2022-03-20 16:59:42 +0200

    Свойство data()

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,6 +2,9 @@
 <div>
     blablabla
    <SinglePostComponent></SinglePostComponent>
+    <div>
+        Name: {{name}}
+    </div>
 </div>
 </template>
 
@@ -11,6 +14,14 @@
 import SinglePostComponent from "./SinglePostComponent";
 export default {
     name: "PostComponent",
+
+    data() {
+        return {
+            name: "vadym",
+            age: 30
+        }
+    },
+
     components: {SinglePostComponent},
     comments: {
         SinglePostComponent
