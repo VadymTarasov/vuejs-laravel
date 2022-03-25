e27d87d7f991dffa8c6db689d395b4fae5131601
vadym <vadym.tarasov.91@gmail.com>
2022-03-25 20:03:12 +0200

    Axios асинхронный запрос обновление персоны

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -12,19 +12,19 @@
             </thead>
             <tbody>
             <template v-for="person in people">
-            <tr>
+            <tr :class="isEdit(person.id) ? 'd-none' : '' ">
                 <th scope="row">{{person.id}}</th>
                 <td>{{person.name}}</td>
                 <td>{{person.age}}</td>
                 <td>{{person.job}}</td>
-                <td><a href="#" @click.prevent="changeEditPersonId(person.id)" class="btn btn-success">edit</a> </td>
+                <td><a href="#" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)" class="btn btn-success">edit</a> </td>
                 </tr>
                 <tr :class="isEdit(person.id) ? '' : 'd-none' ">
                     <th scope="row">{{person.id}}</th>
-                    <td><input type="text" class="form-control"></td>
-                    <td><input type="number" class="form-control"></td>
-                    <td><input type="text" class="form-control"></td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(null)" class="btn btn-success">update</a> </td>
+                    <td><input type="text" v-model="name" class="form-control"></td>
+                    <td><input type="number" v-model="age" class="form-control"></td>
+                    <td><input type="text" v-model="job" class="form-control"></td>
+                    <td><a href="#" @click.prevent="updatePerson(person.id)" class="btn btn-success">update</a> </td>
                 </tr>
             </template>
             </tbody>
@@ -38,7 +38,10 @@ export default {
     data() {
         return {
             people: null,
-            editPersonId: null
+            editPersonId: null,
+            name: null,
+            age: null,
+            job: null,
 
         }
     },
@@ -52,8 +55,21 @@ export default {
                 this.people = res.data
             })
         },
-        changeEditPersonId(id){
+        updatePerson(id){
+            this.editPersonId = null
+            axios.patch(`/api/people/${id}`, {name: this.name, age: this.age, job: this.job})
+                    .then(res =>{
+                        this.getPeople()
+                        // console.log(res);
+                        // this.people = res.data
+                    })
+            },
+        changeEditPersonId(id, name, age, job){
             this.editPersonId =id
+            this.name = name
+            this.age = age
+            this.job = job
+
         },
         isEdit(id){
             return this.editPersonId === id
--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['namespace' => 'Person', 'prefix'=>'people'], function (){
     Route::post('/', [\App\Http\Controllers\Person\StoreController::class, '__invoke']);
     Route::get('/', [\App\Http\Controllers\Person\IndexController::class, '__invoke']);
+    Route::patch('/{person}', [\App\Http\Controllers\Person\UpdateController::class, '__invoke']);
 
 });
