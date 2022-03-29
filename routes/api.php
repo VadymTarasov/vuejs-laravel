75d2b5e5eeae8cbbfddfa11fccc6b028e27acbb7
vadym <vadym.tarasov.91@gmail.com>
2022-03-29 16:59:10 +0300

    Edit компонент и динамичные параметры в vue router

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['namespace' => 'Person', 'prefix'=>'people'], function () {
     Route::post('/', [\App\Http\Controllers\Person\StoreController::class, '__invoke']);
     Route::get('/', [\App\Http\Controllers\Person\IndexController::class, '__invoke']);
+    Route::get('/{person}', [\App\Http\Controllers\Person\ShowController::class, '__invoke']);
     Route::patch('/{person}', [\App\Http\Controllers\Person\UpdateController::class, '__invoke']);
     Route::delete('/{person}', [\App\Http\Controllers\Person\DeleteController::class, '__invoke']);
 
